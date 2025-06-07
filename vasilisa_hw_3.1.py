44ba5837d634a50eee4a387272b721213be64c63
vasechkin <vasechkin@gmail.com>
2025-06-07 15:09:39 +0300

    feat: создан конструктор класса Animals

---
+++ vasilisa_hw_3.1.py
@@ -0,0 +1,7 @@
+class Animals:
+    def __init__(self, name: str, 
+                 type_of_animal: str,
+                 age: int):
+        self.name = name
+        self.type_of_animal = type_of_animal
+        self.age = age
