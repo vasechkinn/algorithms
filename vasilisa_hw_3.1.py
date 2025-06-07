d3e82914f9e31f243bea9d6af9ea5660d90b065a
vasechkin <vasechkin@gmail.com>
2025-06-07 15:29:15 +0300

    feat: создан метод  класса sound и магический метод str

--- vasilisa_hw_3.1.py
+++ vasilisa_hw_3.1.py
@@ -1,7 +1,19 @@
 class Animals:
     def __init__(self, name: str, 
                  type_of_animal: str,
-                 age: int):
+                age: int
+                ):
         self.name = name
         self.type_of_animal = type_of_animal
         self.age = age
+
+    def __str__(self):
+        return f"{self.name, self.type_of_animal, self.age}"
+    
+    def sound_animal(sound):
+        return sound
+    
+cat = Animals('Caticat', 'cat', 5)
+sound = Animals.sound_animal(input('input sound: '))
+print(cat)
+print(sound)
