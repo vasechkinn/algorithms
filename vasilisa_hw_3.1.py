2bda5f633169211a0f34a12effcd5e2aefda0d95
vasechkin <vasechkin@gmail.com>
2025-06-09 20:05:21 +0300

    feat: класс исполнитель

--- vasilisa_hw_3.1.py
+++ vasilisa_hw_3.1.py
@@ -51,64 +51,71 @@
 # print(book1)
 
 # 3 **************************
-class PassengerPlane:
-    def __init__(self, manufacturer: str,
-                 model: str,
-                 capacity: int,
-                 number_passengers: int,
-                 height: int,
-                 speed: int):
-        self.manufacturer = manufacturer
-        self.model = model
-        self.capacity = capacity
-        self.number_passengers = number_passengers
-        self.height = height
-        self.speed = speed
+# class PassengerPlane:
+#     def __init__(self, manufacturer: str,
+#                  model: str,
+#                  capacity: int,
+#                  number_passengers: int,
+#                  height: int,
+#                  speed: int):
+#         self.manufacturer = manufacturer
+#         self.model = model
+#         self.capacity = capacity
+#         self.number_passengers = number_passengers
+#         self.height = height
+#         self.speed = speed
 
-    def __str__(self):
-        return f"{self.manufacturer, self.model, self.capacity, self.number_passengers, self.height, self.speed}"
+#     def __str__(self):
+#         return f"{self.manufacturer, self.model, self.capacity, self.number_passengers, self.height, self.speed}"
 
-    def takeoff(self):
-        if self.height == 0:
-            print('самолет взлетает')
-        else:
-            print('высота больше 0, самолет уже взлетел')
+#     def takeoff(self):
+#         if self.height == 0:
+#             print('самолет взлетает')
+#         else:
+#             print('высота больше 0, самолет уже взлетел')
 
-    def land(self):
-        if self.height != 0:
-            print('начало посадки')
-        else:
-            print('самолет уже приземлился')
+#     def land(self):
+#         if self.height != 0:
+#             print('начало посадки')
+#         else:
+#             print('самолет уже приземлился')
 
-    def height_change(self, delta_height):
-        if self.height > 0:
+#     def height_change(self, delta_height):
+#         if self.height > 0:
 
-            if (delta_height + self.height) >= 0 and (delta_height + self.height) <= 12000:
-                self.height = delta_height + self.height
-                print('новая высота: ', self.height)
-            else:
-                print('недопустимая высота')
+#             if (delta_height + self.height) >= 0 and (delta_height + self.height) <= 12000:
+#                 self.height = delta_height + self.height
+#                 print('новая высота: ', self.height)
+#             else:
+#                 print('недопустимая высота')
 
-        else:
-            print('самолет еще не взлетел')
+#         else:
+#             print('самолет еще не взлетел')
 
-    def speed_change(self, delta_speed): # я с вами не согл, скорость меняется с момента, когда самолет начинает подготовку визлету
-        if self.speed > 0:
+#     def speed_change(self, delta_speed): # я с вами не согл, скорость меняется с момента, когда самолет начинает подготовку визлету
+#         if self.speed > 0:
 
-            if (delta_speed + self.speed) >= 0 and (delta_speed + self.speed)  <= 1100:
-                self.speed = delta_speed + self.speed
-                print('новая скорость: ', self.speed)
-            else:
-                print('fatal speed((((')
+#             if (delta_speed + self.speed) >= 0 and (delta_speed + self.speed)  <= 1100:
+#                 self.speed = delta_speed + self.speed
+#                 print('новая скорость: ', self.speed)
+#             else:
+#                 print('fatal speed((((')
 
-        else:
-            print('заведите самолет')
+#         else:
+#             print('заведите самолет')
 
-plane1 = PassengerPlane('manufacturer', 'model', 15, 9, 2000, 150)
-print(plane1)
+# plane1 = PassengerPlane('manufacturer', 'model', 15, 9, 2000, 150)
+# print(plane1)
 
-speed = plane1.speed_change(500)
-height = plane1.height_change(80000)
+# speed = plane1.speed_change(500)
+# height = plane1.height_change(80000)
 
-takeoff = PassengerPlane.takeoff(plane1)
-landing = PassengerPlane.land(plane1)
+# takeoff = PassengerPlane.takeoff(plane1)
+# landing = PassengerPlane.land(plane1)
+
+# 4 ******************************************
+class Executor:
+    def __init__(self, name: str, surname: str, year_beginning_career: int):
+        self.name = name
+        self.surname = surname
+        self.year_beginning_career = year_beginning_career
