e0727038a6b205d766479fc4bef222ff9478ec3e
vasechkin <vasechkin@gmail.com>
2025-06-09 19:44:49 +0300

    fix: исправлены все ошибки дмитрий

--- vasilisa_hw_3.1.py
+++ vasilisa_hw_3.1.py
@@ -19,86 +19,96 @@
 # sound = cat.play_sound(input('input sound: '))
 
 # 2 *************************************************
-class Autor:
-    def __init__(self, name: str, surname: str):
-        self.name = name
-        self.surname = surname 
+# class Autor:
+#     def __init__(self, name: str, surname: str):
+#         self.name = name
+#         self.surname = surname 
 
-    def __str__(self):
-        return f"{self.name, self.surname}"
+#     def __str__(self):
+#         return f"{self.name, self.surname}"
     
 
-class Book:
-    def __init__(self, name_book: str, autor: Autor,
-                 pages: int):
-        self.name_book = name_book
-        self.autor = autor
-        self.pages = pages
+# class Book:
+#     def __init__(self, name_book: str, autor: Autor,
+#                  pages: int):
+#         self.name_book = name_book
+#         self.autor = autor
+#         self.pages = pages
 
-    def open_page(self, num_str):
-        if self.pages <= num_str and self.pages >= 0:
-            return f'страница успешно открыта'
-        else:
-            return f'fatal((('
+#     def open_page(self, num_str):
+#         if self.pages <= num_str and self.pages >= 0:
+#             return f'страница успешно открыта'
+#         else:
+#             return f'fatal((('
         
-    def __str__(self):
-        return f"{self.name_book, str(self.autor), self.pages}"
+#     def __str__(self):
+#         return f"{self.name_book, str(self.autor), self.pages}"
     
-au = Autor('NO', 'Name')
-print('фвтор:', au)
+# au = Autor('NO', 'Name')
+# print('фвтор:', au)
 
-book1 = Book('new_book', au, 50) #спросить дмитрия про вывод автора
-print(book1)
+# book1 = Book('new_book', au, 50) #спросить дмитрия про вывод автора
+# print(book1)
 
 # 3 **************************
 class PassengerPlane:
-    def __init__(self, aircraft_manufacturer: str,
-                 aircraft_model: str,
-                 aircraft_capacity: int,
+    def __init__(self, manufacturer: str,
+                 model: str,
+                 capacity: int,
                  number_passengers: int,
-                 current_height: int,
-                 current_speed: int):
-        self.aircraft_manufacturer = aircraft_manufacturer
-        self.aircraft_model = aircraft_model
-        self.aircraft_capacity = aircraft_capacity
+                 height: int,
+                 speed: int):
+        self.manufacturer = manufacturer
+        self.model = model
+        self.capacity = capacity
         self.number_passengers = number_passengers
-        self.current_height = current_height
-        self.current_speed = current_speed
+        self.height = height
+        self.speed = speed
 
     def __str__(self):
-        return f"{self.aircraft_manufacturer, self.aircraft_model, self.aircraft_capacity, self.number_passengers, self.current_height, self.current_speed}"
+        return f"{self.manufacturer, self.model, self.capacity, self.number_passengers, self.height, self.speed}"
 
-    def airplane_takeoff(self):
-        if self.current_height == 0:
+    def takeoff(self):
+        if self.height == 0:
             print('самолет взлетает')
         else:
             print('высота больше 0, самолет уже взлетел')
 
-    def landing_the_plane(self):
-        if self.current_height != 0:
+    def land(self):
+        if self.height != 0:
             print('начало посадки')
         else:
             print('самолет уже приземлился')
 
-    def height_change(self, height):
-        if height >= 0 and height <= 12000:
-            self.current_height = height
-            print('новая высота: ', self.current_height)
-        else:
-            print('недопустимая высота')
+    def height_change(self, delta_height):
+        if self.height > 0:
 
-    def speed_change(self, speed):
-        if speed >= 0 and speed <= 1100:
-            self.current_speed = speed
-            print('новая скорость: ', self.current_speed)
+            if (delta_height + self.height) >= 0 and (delta_height + self.height) <= 12000:
+                self.height = delta_height + self.height
+                print('новая высота: ', self.height)
+            else:
+                print('недопустимая высота')
+
         else:
-            print('fatal speed((((')
+            print('самолет еще не взлетел')
+
+    def speed_change(self, delta_speed): # я с вами не согл, скорость меняется с момента, когда самолет начинает подготовку визлету
+        if self.speed > 0:
+
+            if (delta_speed + self.speed) >= 0 and (delta_speed + self.speed)  <= 1100:
+                self.speed = delta_speed + self.speed
+                print('новая скорость: ', self.speed)
+            else:
+                print('fatal speed((((')
+
+        else:
+            print('заведите самолет')
 
 plane1 = PassengerPlane('manufacturer', 'model', 15, 9, 2000, 150)
 print(plane1)
 
-speed = PassengerPlane.speed_change(plane1, 500)
-height = PassengerPlane.height_change(plane1, 80000)
+speed = plane1.speed_change(500)
+height = plane1.height_change(80000)
 
-takeoff = PassengerPlane.airplane_takeoff(plane1)
-landing = PassengerPlane.landing_the_plane(plane1)
+takeoff = PassengerPlane.takeoff(plane1)
+landing = PassengerPlane.land(plane1)
