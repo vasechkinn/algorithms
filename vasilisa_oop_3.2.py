9802709e603848fc2076073ab3a21678a9b3c47a
vasechkin <vasechkin@gmail.com>
2025-06-17 23:59:18 +0300

    feat: сделала две одинаковых функции, исправила...........

--- vasilisa_oop_3.2.py
+++ vasilisa_oop_3.2.py
@@ -30,4 +30,9 @@ class Vector2D:
     
     def __add__(self, other: 'Vector2D'):
         if self == other:
+            return Vector2D(self.start, Point(self.end.x + other.end.x, self.end.y + other.end.y))
+        
+    def __sub__(self, other: 'Vector2D'):
+        if self == other:
             return Vector2D(self.start, Point(self.end.x - other.end.x, self.end.y - other.end.y))
+        
