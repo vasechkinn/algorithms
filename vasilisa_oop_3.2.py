dabe8487c7d32344492b8fe8bb829035853cdf4e
vasechkin <vasechkin@gmail.com>
2025-06-17 23:42:08 +0300

    feat: class point, функции add, sub

---
+++ vasilisa_oop_3.2.py
@@ -0,0 +1,26 @@
+# 1 **********************************************************
+# не выкупила как делать векторы без точек... класс точек сделала первым
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other: 'Point'):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: 'Point'):
+        return Point(self.x - other.x, self.y - other.y)
+    
+    def __str__(self):
+        return f"point{self.x, self.y}"
+    
+A = Point(1, 1)
+B = Point(2, 2)
+
+print(A + B)
+print(A - B)
+
+class Vector2D:
+    def __init__(self, start: Point, end: Point):
+        self.start = start
+        self.end = end
