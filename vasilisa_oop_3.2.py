85552a332a2554067b405f578172830dbb5b9854
vasechkin <vasechkin@gmail.com>
2025-06-18 00:28:36 +0300

    feat: class vector, coord, len

--- vasilisa_oop_3.2.py
+++ vasilisa_oop_3.2.py
@@ -35,9 +35,21 @@ class Vector2D:
     def __sub__(self, other: 'Vector2D'):
         if self == other:
             return Vector2D(self.start, Point(self.end.x - other.end.x, self.end.y - other.end.y))
+        
     def __mul__(self, delta):
         new_start = Point(self.start.x * delta, self.start.y * delta)
         new_end = Point(self.end.x * delta, self.end.y * delta)
         new_vector = Vector2D(new_start, new_end)
 
         return new_vector
+    
+    def coord(self):
+        """
+        координата вектора АВ(xb - xa; yb - ya)
+        """
+        return Point(self.end.x - self.start.x, self.end.y - self.start.y)
+
+    def len(self):
+        cor_vector = self.coord()
+
+        return (cor_vector.x ** 2 + cor_vector.y ** 2) ** 0.5
