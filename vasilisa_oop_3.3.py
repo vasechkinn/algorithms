5e4b0eea9596577214ad09738eae1f4b62af9c32
vasechkin <vasechkin@gmail.com>
2025-06-24 01:34:06 +0300

    feat: трииииии  класса я забыла про коммиты

--- vasilisa_oop_3.3.py
+++ vasilisa_oop_3.3.py
@@ -116,3 +116,108 @@ from __future__ import annotations
 # print(rect.set_height(-15.0))
 # print(rect.get_width())
 # print(rect.area())
+
+# 3 **************************************************************
+class People:
+    def __init__(self, name: str, surname:str, age: int):
+        self.__name = name
+        self.__surname = surname
+        self.__age = age
+
+    def __str__(self):
+        return f"name: {self.__name}\nsurname: {self.__surname}\nage: {self.__age}"
+
+    def get_age(self):
+        return f"age: {self.__age}"
+    
+    def get_name(self):
+        return f"name: {self.__name}"
+    
+    def get_surname(self):
+        return f"surname: {self.__surname}"
+
+    def set_name(self, new_name: str):
+        if isinstance(new_name, str):
+            self.__name = new_name
+        else:
+            raise TypeError("fatal(, ne str")
+        
+    def set_surname(self, new_surname: str):
+        if isinstance(new_surname, str):
+            self.__surname = new_surname
+        else:
+            raise TypeError("fatal(, ne str")
+        
+    def set_age(self, new_age: int):
+        if isinstance(new_age, int):
+
+            if new_age >= 5 and new_age < 20:
+                self.__age = new_age
+            else:
+                print('вы либо слишком юны, либо пора задуматься...')
+
+        else:
+            raise TypeError("fatal(, ne int")
+
+class Journal:
+    def __init__(self, math: list, russian: list, informatics: list):
+        self.__math = math
+        self.__russian = russian
+        self.__informatics = informatics
+
+    def __repr__(self):
+        return (f"math: {self.__math}; "
+                f"russian: {self.__russian}; "
+                f"informatics: {self.__informatics}; ")
+    
+    def mean_math(self):
+        if len(self.__math) == 0:
+            return
+        
+        count = 0
+        for i in range(len(self.__math)):
+            count += self.__math[i]
+
+        return round(count / len(self.__math), 2)
+    
+    def mean_russian(self):
+        if len(self.__russian) == 0:
+            return
+        
+        count = 0
+        for i in range(len(self.__russian)):
+            count += self.__russian[i]
+
+        return round(count / len(self.__russian), 2)
+    
+    def mean_informatics(self):
+        if len(self.__informatics) == 0:
+            return
+        
+        count = 0
+        for i in range(len(self.__informatics)):
+            count += self.__informatics[i]
+
+        return round(count / len(self.__informatics), 2)
+
+class Student:
+    def __init__(self, people: People, class_stud: str, journal: Journal):
+        self.__people = people
+        self.__class_stud = class_stud
+        self.__journal = journal
+
+    def __repr__(self):
+        return (f"student: \n{self.__people}\n"
+                f"class: {self.__class_stud}\n"
+                f"journal: \n{self.__journal}")
+    
+p1 = People('lol', 'kek', 12)
+p1.set_age(15)
+m = [2, 4, 5]
+r = [2, 2, 2]
+inf = [2, 4, 5]
+jour = Journal(m, r, inf)
+print('mean math: ', jour.mean_math())
+
+stud = Student(p1, '2a', jour)
+print(stud)
